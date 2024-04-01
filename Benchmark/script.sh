a746002e30a07d1eb30f6a4433e9cecdeecd39bd
ruhanaazam <razam2@illinois.edu>
2024-04-01 14:31:45 -0700

    Cleaned up script.sh

--- Benchmark/script.sh
+++ Benchmark/script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+gpu=0
 envs=("narea") #"sla" "pn" "ps")
 kernels=("matern52" "matern32" "matern12" "rbf" "spectral-10")
 acqs=("EI" "PI" "UCB-0.1" "UCB-0.2" "UCB-0.5")
@@ -8,13 +9,7 @@ for env in ${envs[@]}; do
     python main.py --env $env --n $n --acq random
     for acq in ${acqs[@]}; do
         for kernel in ${kernels[@]}; do
-            python main.py --env $env --n $n --kernel $kernel --acq $acq 
+            python main.py --gpu $gpu --env $env --n $n --kernel $kernel --acq $acq 
         done
     done
 done
-
-# n=300
-# python main.py --env pn --n $n --kernel matern52 --acq EI
-# python main.py --env pn --n $n --kernel rbf --acq EI
-# python main.py --env pn --n $n --kernel matern12 --acq EI
-
