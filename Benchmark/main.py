ce092cda91541300ffb0f37823c9f1fbe4908476
ruhanaazam <razam2@illinois.edu>
2024-04-02 11:09:16 -0700

    Added tracking of learning curves during training process.

--- Benchmark/main.py
+++ Benchmark/main.py
@@ -36,7 +36,7 @@ def getLookup(trait):
     no_nan_lookup = torch.nan_to_num(lookup_tensor)
     no_nan_lookup[no_nan_lookup > 1] = 0
     return no_nan_lookup
-
+    
 
 def getKernel(kernel_name):
     covar_module = None
@@ -56,7 +56,7 @@ def getKernel(kernel_name):
         print("Not a valid kernel")  # should also throw error
     return covar_module
 
-
+    
 def main():
     parser = argparse.ArgumentParser()
 
@@ -80,14 +80,11 @@ def main():
         runBO(args)
     return
 
-
 def runRandom(args):
     n = args.n  # replace this
     trait = args.env
     seeds = 5  # consider replacing this
     acq_name = "random"
-
-    # get lookup environment
     lookup = getLookup(args.env)
     ub, lb = 2150, 0
 
@@ -132,6 +129,7 @@ def runRandom(args):
             # end timer and add
             toc = time.perf_counter()  # end time
             _result.append([new_Y[0][0].item(), toc - tic, new_X[0]])
+            
 
         # save all your queries
         torch.save(train_X, f"./output/{trait}/botorch{acq_name}_X_{seed}.npy")
@@ -159,9 +157,7 @@ def runRandom(args):
         # print full time
         toc = time.perf_counter()  # end time
         print("BoTorch Took ", (toc - tic), "seconds")
-
     return
-
 
 def getCoordTensor(size=2150):
     x, y = torch.arange(size), torch.arange(size)
@@ -169,6 +165,18 @@ def getCoordTensor(size=2150):
     coordinates_tensor = torch.stack((X, Y), dim=-1).reshape(-1, 2)
     return coordinates_tensor.to(device, torch.float64)
 
+def calcLoss(model, train_X, lookup, **kwargs):
+    seed = kwargs.get('seed', None)
+    torch.manual_seed(seed) 
+    m = 3000 #size of validation set
+    
+    val_X = torch.rand(m, 2, dtype=torch.float64, device=device) * 2150
+    val_Y = model.posterior(val_X).mean.detach()
+    train_Y = model.posterior(train_X).mean.detach()
+    
+    train_loss = torch.mean((train_Y - lookup(train_X))**2)
+    val_loss = torch.mean((val_Y - lookup(val_X))**2)
+    return train_loss, val_loss
 
 def runBO(args):
     num_restarts = 128
@@ -179,16 +187,21 @@ def runBO(args):
     acq_name = args.acq
     kernel_name = args.kernel
     kernel = getKernel(kernel_name)
+  
+    # create function for querying the environment
+    table = getLookup(args.env)
+    def lookup(X):
+        X_indices = X.long().cpu() 
+        Y = table[X_indices[:, 0], X_indices[:, 1]].reshape(-1, 1)
+        return Y.to(device, torch.float64)
 
-    # get lookup environment
-    lookup = getLookup(args.env)
     bounds = torch.stack(
-        [torch.zeros(2).double(), torch.ones(2).double() * (lookup.shape[0] - 1)]
+        [torch.zeros(2).double(), torch.ones(2).double() * (table.shape[0] - 1)]
     ).to(device, torch.float64)
-
+    
     # check the lookup table
-    assert not torch.isnan(torch.sum(lookup))
-    assert not torch.isinf(torch.sum(lookup))
+    assert not torch.isnan(torch.sum(table))
+    assert not torch.isinf(torch.sum(table))
 
     print(f"Running {trait}, {args.acq}-{kernel_name}...")
     for seed in range(0, seeds):  # seed one is already run and stuff
@@ -196,25 +209,13 @@ def runBO(args):
 
         # collect random points as training points
         torch.manual_seed(seed)  # setting seed
-        train_X = torch.rand(10, 2, dtype=torch.float64, device=device) * (
-            lookup.shape[0] - 1
-        )
-        train_Y = torch.tensor(
-            [
-                lookup[int(train_X[i][0]), int(train_X[i][1])]
-                for i in range(0, len(train_X))
-            ],
-            dtype=torch.float64,
-            device=device,
-        )
-        train_Y = train_Y.reshape(-1, 1)
+        train_X = torch.rand(10, 2, dtype=torch.float64, device=device) * 2150
+        train_Y = lookup(train_X)
 
         # main bayes_opt training loop
         _result = []
+        _curve = {"n": [], "train_loss": [] , "val_loss": []}
         for i in tqdm(range(n)):
-            #             if "spectral" in acq_name :
-            #                 kernel = kernel.initialize_from_data(train_X, train_Y)
-
             gp = SingleTaskGP(
                 train_X,
                 train_Y,
@@ -238,13 +239,17 @@ def runBO(args):
             elif acq_name == "KG":  # working
                 num_restarts = 10
                 acq = qKnowledgeGradient(gp)
-            #             elif acq_name == "MES":
+            elif acq_name == "MES":
+                continue
+            # TODO
             #                 x1_values = np.linspace(0, 2150, 2150)
             #                 x2_values = np.linspace(0, 2150, 2150)
             #                 x1, x2 = np.meshgrid(x1_values, x2_values)
             #                 candidates = np.vstack([x1.ravel(), x2.ravel()]).T
             #                 candidates = torch.from_numpy(candidates).to(device=device)
             #                 acq = qMaxValueEntropy(gp, candidates)
+            elif acq_name == "Random":
+                continue
             else:
                 print(f"{acq_name} is not a valid acquisition function")
 
@@ -255,13 +260,15 @@ def runBO(args):
                 num_restarts=num_restarts,
                 raw_samples=raw_samples,
             )
-
-            new_Y = torch.tensor(
-                [lookup[int(new_X[0][0]), int(new_X[0][1])]],
-                dtype=torch.float64,
-                device=device,
-            ).reshape(-1, 1)
-
+            new_Y = lookup(new_X)
+            
+            #calculate validation loss here 
+            kwargs = {"seed": seed,}
+            train_loss, val_loss = calcLoss(gp, train_X, lookup, **kwargs)
+            _curve["train_loss"].append(train_loss.item())
+            _curve["val_loss"].append(val_loss.item())
+            _curve["n"].append(i)
+            
             # add new candidate
             train_X = torch.cat([train_X, new_X])
             train_Y = torch.cat([train_Y, new_Y])
@@ -271,13 +278,14 @@ def runBO(args):
             _result.append([new_Y[0][0].item(), toc - tic, new_X[0]])
 
         # save all your queries
-        torch.save(
-            train_X, f"./output/{trait}/botorch{acq_name}_{kernel_name}_X_{seed}.npy"
-        )
-        torch.save(
-            train_Y, f"./output/{trait}/botorch{acq_name}_{kernel_name}_Y_{seed}.npy"
-        )
-
+        torch.save(train_X, f"./output/{trait}/botorch{acq_name}_{kernel_name}_X_{seed}.npy")
+        torch.save(train_Y, f"./output/{trait}/botorch{acq_name}_{kernel_name}_Y_{seed}.npy")
+        
+        #save learning curve
+        curve_df = pd.DataFrame(_curve)
+        curve_df.to_csv(f"./output/{trait}/botorch{acq_name}_{kernel_name}_curve_{seed}.npy",
+            encoding="utf8",)
+        
         # organize the list to have running best
         best = [0, 0, 0]  # format is [time, best co-heritabilty]
         botorch_result = []
@@ -301,6 +309,7 @@ def runBO(args):
         # print full time
         toc = time.perf_counter()  # end time
         print("BoTorch Took ", (toc - tic), "seconds")
-
+        
+    return
 
 main()
