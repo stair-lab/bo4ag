ea42f2965b8dbdcfa84da12c204fafd4415158f9
ruhanaazam <razam2@illinois.edu>
2024-04-11 16:41:40 -0700

    Added box_cox, standardize and log transformations to the getLookup() as input transforms

--- Benchmark/gp_fit.py
+++ Benchmark/gp_fit.py
@@ -3,27 +3,53 @@ import pandas as pd
 from tqdm import tqdm
 import time
 import os
+import argparse
 
 from botorch.models import SingleTaskGP
 from botorch.fit import fit_gpytorch_mll
 from gpytorch.mlls import ExactMarginalLogLikelihood
 from botorch.models.transforms.outcome import Standardize
 from botorch.models.transforms import Normalize
-import argparse
+from scipy import stats
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+def stadardize(data):
+    mean = data.mean()
+    std = data.std()    
+    standardized_data = (data - mean) / std
+    return standardized_data
 
 def getLookup(trait, transform=None):
     path = f"./data/{trait}_coh2.csv"
     lookup = pd.read_csv(path, header=0)
 
+    # Replace nans with zero
     lookup_tensor = torch.tensor(lookup.values, dtype=torch.float64)
     no_nan_lookup = torch.nan_to_num(lookup_tensor)
     no_nan_lookup[no_nan_lookup > 1] = 0
     
-    #if transform...
-    return no_nan_lookup
+    # Make zeros, non-zero
+    mask = no_nan_lookup == 0
+    no_nan_lookup[mask] = 10e-6
+    
+    # Apply input transforms
+    if transform == "log":
+        log_data = torch.log(no_nan_lookup)
+        return stadardize(log_data) #standardize
+    elif transform == "box_cox":
+        flat_lookup = no_nan_lookup.reshape(-1)
+        fitted_data, fitted_lambda = stats.boxcox(flat_lookup)
+        unflat_lookup = no_nan_lookup.reshape(no_nan_lookup.shape)
+        
+        print((unflat_lookup - no_nan_lookup))
+        print(no_nan_lookup)
+        exit()
+        return stadardize(unflat_lookup) #standardize
+    elif transform is not None:
+        print(f"{transform} is not a valid input tranform...")
+    
+    return stadardize(no_nan_lookup) #standardize
 
 def calcMSE(model, train_X, lookup, **kwargs):
     seed = kwargs.get("seed", None)
@@ -72,13 +98,19 @@ def main(args):
     seeds = 5
     n = args.n
     trait = args.env  # options are narea, sla, pn, ps
+    transform = args.transform
+    
+    # create function to querying the environment
+    table = getLookup(trait, transform)
 
     # directory to save results
     timestamp = time.strftime("%Y%m%d-%H%M%S")
     os.mkdir(f"./output/{timestamp}")
-
-    # create function to querying the environment
-    table = getLookup(trait)
+    
+    #save args in a file
+    with open(f"./output/{timestamp}/args.txt", 'w') as file:
+        for key, value in vars(args).items():
+            file.write(f"{key}: {value}\n")
     
     def lookup(X):
         X_indices = X.long().cpu()
@@ -91,8 +123,6 @@ def main(args):
         #query all random points
         full_train_X = torch.rand(n+10, 2, dtype=torch.float64, device=device) * 2150
         full_train_Y = lookup(full_train_X)
-        print(full_train_X[:20])
-        exit()
 
         # main loop
         _result = {"n": [], "train_loss": [], "val_loss": [], "best": []}
@@ -104,7 +134,7 @@ def main(args):
             gp = SingleTaskGP(
                 train_X,
                 train_Y,
-                outcome_transform=Standardize(1),
+                #outcome_transform=Standardize(1), moved standardize to the data loading part! (refer to getLookup())
                 input_transform=Normalize(train_X.shape[-1]),
             )
             mll = ExactMarginalLogLikelihood(gp.likelihood, gp)
@@ -120,12 +150,12 @@ def main(args):
             _result["best"].append(max(train_Y).item())
             # print(f"train loss: {train_loss}, validation loss: {val_loss}")
 
-#         # save results
-#         _result = pd.DataFrame(_result)
-#         _result.to_csv(
-#             f"./output/{timestamp}/gp_{seed}.npy",
-#             encoding="utf8",
-#         )
+        # save result
+        _result = pd.DataFrame(_result)
+        _result.to_csv(
+            f"./output/{timestamp}/gp_{seed}.npy",
+            encoding="utf8",
+        )
     return
 
 
@@ -134,7 +164,13 @@ if __name__ == "__main__":
     parser.add_argument("--env", help="Environment to run search.")
     parser.add_argument("--n", type=int, default=300, help="Number of random points in training set")
     parser.add_argument("--m", type=int, default=3000, help="Number of random points in validation set")
+    parser.add_argument("--transform", default=None, help="Transforming on the search space")
     #parser.add_argument("--loss", default="MSE", help="=Type of loss function")
     parser.add_argument("--gpu", type=int, default=0, help="Gpu id to run the job")
     args = parser.parse_args()
+    
+    #set global device
+    global device
+    device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
+    
     main(args)
