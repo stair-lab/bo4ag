e48f09754d11cbc87d27b09c41fd8b7e5d1f51e1
ruhanaazam <razam2@illinois.edu>
2024-04-18 13:56:01 -0700

    Added data_util.py file for data formatting and loading purposed.

---
+++ Benchmark/data_util.py
@@ -0,0 +1,38 @@
+import torch
+import pandas as pd
+from scipy import stats
+
+def stadardize(data):
+    mean = data.mean()
+    std = data.std()    
+    standardized_data = (data - mean) / std
+    return standardized_data
+
+def getLookup(trait, transform=None):
+    path = f"./data/{trait}_coh2.csv"
+    lookup = pd.read_csv(path, header=0)
+
+    # Replace nans with zero
+    lookup_tensor = torch.tensor(lookup.values, dtype=torch.float64)
+    no_nan_lookup = torch.nan_to_num(lookup_tensor)
+    no_nan_lookup[no_nan_lookup > 1] = 0
+    
+    # Make zeros, non-zero
+    mask = no_nan_lookup == 0
+    no_nan_lookup[mask] = 10e-6
+    
+    # Apply input transforms
+    if transform == "log":
+        log_data = torch.log(no_nan_lookup)
+        return stadardize(log_data) #standardize
+    elif transform == "box_cox":
+        flat_lookup = no_nan_lookup.reshape(-1)
+        fitted_data, fitted_lambda = stats.boxcox(flat_lookup)
+        unflat_lookup = torch.from_numpy(fitted_data).reshape(2151, 2151)
+        return stadardize(unflat_lookup) #standardize
+    elif transform == "standardized":
+        return stadardize(no_nan_lookup) #standardize
+    elif transform is not None:
+        print(f"{transform} is not a valid input tranform...")
+        
+    return no_nan_lookup #standardize
--- Benchmark/gp_fit.py
+++ Benchmark/gp_fit.py
@@ -11,43 +11,10 @@ from botorch.fit import fit_gpytorch_mll, fit_fully_bayesian_model_nuts
 from gpytorch.mlls import ExactMarginalLogLikelihood
 from botorch.models.transforms.outcome import Standardize
 from botorch.models.transforms import Normalize
-from scipy import stats
+
+from data_util import stadardize, getLookup
 
 #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-def stadardize(data):
-    mean = data.mean()
-    std = data.std()    
-    standardized_data = (data - mean) / std
-    return standardized_data
-
-def getLookup(trait, transform=None):
-    path = f"./data/{trait}_coh2.csv"
-    lookup = pd.read_csv(path, header=0)
-
-    # Replace nans with zero
-    lookup_tensor = torch.tensor(lookup.values, dtype=torch.float64)
-    no_nan_lookup = torch.nan_to_num(lookup_tensor)
-    no_nan_lookup[no_nan_lookup > 1] = 0
-    
-    # Make zeros, non-zero
-    mask = no_nan_lookup == 0
-    no_nan_lookup[mask] = 10e-6
-    
-    # Apply input transforms
-    if transform == "log":
-        log_data = torch.log(no_nan_lookup)
-        return stadardize(log_data) #standardize
-    elif transform == "box_cox":
-        flat_lookup = no_nan_lookup.reshape(-1)
-        fitted_data, fitted_lambda = stats.boxcox(flat_lookup)
-        unflat_lookup = torch.from_numpy(fitted_data).reshape(2151, 2151)
-        return stadardize(unflat_lookup) #standardize
-    elif transform is not None:
-        print(f"{transform} is not a valid input tranform...")
-        
-    
-    return stadardize(no_nan_lookup) #standardize
 
 def calcMSE(model, train_X, lookup, **kwargs):
     seed = kwargs.get("seed", None)
@@ -136,8 +103,10 @@ def main(args):
                     outcome_transform=Standardize(1),
                     input_transform=Normalize(train_X.shape[-1])
                 )
-                fit_fully_bayesian_model_nuts(gp)
-
+                fit_fully_bayesian_model_nuts(gp,
+                                             max_tree_depth=5,
+                                              num_samples=128,
+                                              warmup_steps=50,)
             else:
                 gp = SingleTaskGP(
                     train_X,
@@ -156,7 +125,7 @@ def main(args):
             _result["val_loss"].append(val_loss.item())
             _result["n"].append(i)
             _result["best"].append(max(train_Y).item())
-            # print(f"train loss: {train_loss}, validation loss: {val_loss}")
+            print(f"train loss: {train_loss}, validation loss: {val_loss}")
 
         # save result
         _result = pd.DataFrame(_result)
